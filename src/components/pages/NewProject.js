65ca7d5b8d7baaabcae316abc5bc749d6138b4e9
Bruno Fioravanti <bruno.fioravanti2@gmail.com>
2022-05-04 23:43:31 -0300

    Progresso do final do dia 04/05/2022

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,9 +1,14 @@
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
+
+
 function NewProject() {
     return (
-        <div>
+        <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <p>Formulário</p>
+            <ProjectForm btnText="Criar projeto" />
         </div>
     )
 }
